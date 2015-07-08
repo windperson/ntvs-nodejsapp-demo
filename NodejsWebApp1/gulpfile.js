2ebbd84ca2463e52da9d0e339e4ae53540ea5da9
Isak-Pao <isak-pao@capcom.com>
2015-07-08 18:10:26 +0800

    gulp-tsc will generate not-working sourcemap file

--- NodejsWebApp1/gulpfile.js
+++ NodejsWebApp1/gulpfile.js
@@ -1,5 +1,7 @@
 ﻿var gulp = require('gulp');
 var tsc = require('gulp-tsc');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 var runseq = require('run-sequence');
 var del = require('del');
 var debug = require('gulp-debug');
@@ -9,9 +11,10 @@ var paths = {
     typescripts : {
         src : ['Scripts/**/*.d.ts', 'src/**/*.ts'],
         out : ['src/**/*.js', 'src/**/*.js.map'],
-        dev_dest : './',
+        dev_dest : '.',
         package_dest : 'build',
-        package_clean : ['build/**/*']
+        package_clean : ['build/**/*'],
+        package_out : ['build/**/*.js']
     }
 };
 
@@ -20,7 +23,10 @@ gulp.task('compile:typescript', function () {
     .pipe(tsc({
         module: "CommonJS",
         target: "ES5",
-        sourcemap: true
+        removeComments: true,
+        sourcemap: true,
+        sourceRoot: "",
+        outDir : "src"
     })).pipe(gulp.dest(paths.typescripts.dev_dest));
 });
 
@@ -40,8 +46,21 @@ gulp.task('package', function () {
     .pipe(debug({ title: 'dest:' }))
 });
 
+gulp.task('compress', function () {
+    return gulp.src(paths.typescripts.package_out).pipe(debug({ title: 'origin:' }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify()).pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.typescripts.package_dest))
+    .pipe(debug({ title: 'compressed:' }));
+});
+
 gulp.task('build', function (callback) {
     return runseq('clean', 'compile:typescript', 'package', callback);
 });
 
-gulp.task('default', ['compile:typescript'])
+gulp.task('build:compress', function (callback) {
+    return runseq('clean', 'compile:typescript', 'package', 'compress', callback);
+});
+
+
+gulp.task('default', ['clean','compile:typescript'])
